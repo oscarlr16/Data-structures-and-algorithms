9c5da925294dd5d83e9f2345b81a49c14df5dbf7
oscarlr16 <oscarleon.ro16@gmail.com>
2023-11-18 21:42:23 -0600

    feat: implement right rotation function and update displacement value in array rotation

--- Data-structures-and-algorithms/Array/Rotation-of-array/rotation-of-array.cpp
+++ Data-structures-and-algorithms/Array/Rotation-of-array/rotation-of-array.cpp
@@ -7,7 +7,7 @@ int main(){
 
     int array[] = {0,1,2,3,4,5,6};
     int number_of_elements = sizeof(array) / sizeof(array[0]);
-    int displacement_number = 5;
+    int displacement_number = 10;
 
     std::cout << "Original array: ";
     for(int i = 0; i < number_of_elements; i++){
@@ -15,14 +15,22 @@ int main(){
     }
     std::cout << std::endl;
 
-    std::cout << "Left rotation: ";
-    int* aux_array = left_rotate(array, displacement_number, number_of_elements);
+    std::cout << "Left rotation:  ";
+    int* aux_array_1 = left_rotate(array, displacement_number, number_of_elements);
     for(int i = 0; i < number_of_elements; i++){
-        std::cout << aux_array[i] << " ";
+        std::cout << aux_array_1[i] << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Right rotation: ";
+    int* aux_array_2 = right_rotate(array, displacement_number, number_of_elements);
+    for(int i = 0; i < number_of_elements; i++){
+        std::cout << aux_array_2[i] << " ";
     }
     std::cout << std::endl;
     
-    release_memory(aux_array);
+    release_memory(aux_array_1);
+    release_memory(aux_array_2);
 
     return 0;
 }
@@ -39,7 +47,14 @@ int* left_rotate(int arr[], int displacement_number, int number_of_elements){
 }
 
 int* right_rotate(int arr[], int displacement_number, int number_of_elements){
-    return 0;
+    displacement_number = displacement_number % number_of_elements;
+    int* new_array = new int[number_of_elements];
+
+    for (int i = 0; i < number_of_elements; i++) {
+        new_array[i] = arr[(i + (number_of_elements - displacement_number)) % number_of_elements];
+    } 
+
+    return new_array;
 }
 
 
