23aae4f92777547992e7454d2710fa91bd568377
oscarlr16 <oscarleon.ro16@gmail.com>
2023-11-20 13:54:47 -0600

    range-queries-in-the-array

---
+++ Data-structures-and-algorithms/Array/Range-queries-in-the-array/range-queries-in-the-array.cpp
@@ -0,0 +1,21 @@
+/*
+Requerimientos:
+Tienes un array de enteros.
+Necesitas responder a varias consultas. Cada consulta te pide encontrar el valor mínimo o máximo en un subconjunto específico del array.
+Las consultas especifican los índices de inicio y fin del rango en el que debes buscar el mínimo o máximo.
+Ejemplo:
+Input:
+Array: 
+[2,6,4,8,10,9,15]
+Consultas:
+Encuentra el mínimo entre los índices 1 y 4.
+Encuentra el máximo entre los índices 3 y 6.
+Encuentra el mínimo entre los índices 0 y 2.
+Output Esperado:
+Para la consulta 1: El mínimo entre los índices 1 y 4 es 4.
+Para la consulta 2: El máximo entre los índices 3 y 6 es 15.
+Para la consulta 3: El mínimo entre los índices 0 y 2 es 2.
+Nota:
+Los índices en las consultas están basados en 0, es decir, el índice 0 corresponde al primer elemento del array.
+Asume que las consultas siempre tendrán índices válidos dentro del rango del array.
+*/
